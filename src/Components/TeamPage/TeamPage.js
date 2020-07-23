a77f6eacb391d138c6a9b22414a4dce001973bec
RichardEsquivel <rlesquivel@gmail.com>
2020-07-23 10:47:33 -0500

    Updated team page

--- src/Components/TeamPage/TeamPage.js
+++ src/Components/TeamPage/TeamPage.js
@@ -7,14 +7,14 @@ import { Link } from "@material-ui/core";
 
 import { data17, data19 } from "./TeamPageData.js";
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles((theme) => ({
   teamHeader: {
     width: "100%",
     backgroundColor: "black",
     color: "white",
     fontFamily: "Titillium Web",
     fontWeight: "bold",
-    fontSize: "3.0rem"
+    fontSize: "3.0rem",
   },
   profileContainer: {
     margin: theme.spacing(1),
@@ -22,48 +22,48 @@ const useStyles = makeStyles(theme => ({
     display: "flex",
     flexWrap: "wrap",
     margin: "0 auto",
-    width: "70%"
+    width: "70%",
   },
   profilePicsMargin: {
     marginBottom: "12%",
-    display: "flex"
+    display: "flex",
   },
   headerStyles: {
     width: "100%",
     height: "100%",
-    fontFamily: "Titillium Web"
+    fontFamily: "Titillium Web",
   },
   dataContainer: {
     display: "flex",
     flexDirection: "column",
     marginLeft: "10px",
-    width: "40%"
+    width: "40%",
   },
   image: {
     borderRadius: "50%",
     "&:hover": {
       WebkitTransform: "scale(1.1)",
       WebkitBoxShadow: " 0px 0px 15px #ccc",
-      transition: ".5s"
-    }
+      transition: ".5s",
+    },
   },
   name: {
     color: "white",
     margin: 0,
-    fontSize: ".9rem"
+    fontSize: ".9rem",
   },
   role: {
     color: "white",
     margin: 0,
-    fontSize: ".8rem"
-  }
+    fontSize: ".8rem",
+  },
 }));
 
 export default function TeamPage() {
   const props = useSpring({
     to: { opacity: 1, marginLeft: 0 },
     from: { opacity: 0, marginLeft: -1200 },
-    config: { duration: 800 }
+    config: { duration: 800 },
   });
   const classes = useStyles();
   return (
@@ -73,11 +73,11 @@ export default function TeamPage() {
       </div>
       <div className={classes.teamHeader}>
         <h3>Come Meet the Teams!</h3>
-        <h3>Lab 17</h3>
+        <h3>Lab 19 Fullstack and DS</h3>
       </div>
       <animated.div style={props}>
         <Grid className={classes.profileContainer}>
-          {data17.map(e => (
+          {data17.map((e) => (
             <Grid
               className={classes.profilePicsMargin}
               item
@@ -98,11 +98,11 @@ export default function TeamPage() {
 
         <div>
           <div className={classes.teamHeader}>
-            <h3>Lab 19</h3>
+            <h3>Lab 17 DS/UX</h3>
           </div>
 
           <Grid className={classes.profileContainer}>
-            {data19.map(e => (
+            {data19.map((e) => (
               <Grid
                 className={classes.profilePicsMargin}
                 item
